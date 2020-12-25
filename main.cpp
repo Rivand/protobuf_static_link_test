a6faaba9084f6f25d2c61436e5cde9a0e60a1443
Rivand <RivandBlack@yandex.ru>
2020-12-25 19:01:57 +0500

    v 0.0.2
    - WIP.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
-
 using std::cout;
 using std::endl;
 using std::thread;
+using namespace std::chrono_literals;
 
-
-int main (){
-    thread th {[](){
-            cout << "Second thread." << endl;
-    }};
-    cout << "First thread." << endl;
+int main() {
+    thread th([]() -> void {
+        std::this_thread::sleep_for(1s);
+        cout << "Some text." << endl;
+    });
     th.join();
-    cout << "End." << endl;
+    cout << "End" << endl;
     return 0;
 }
